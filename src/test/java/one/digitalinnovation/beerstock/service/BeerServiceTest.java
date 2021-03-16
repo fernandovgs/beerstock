9372ade3bdaf2586c2d29c0a876b56e443e72041
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-03-15 22:31:41 -0300

    Assert Throw Exception for registered beer

--- src/test/java/one/digitalinnovation/beerstock/service/BeerServiceTest.java
+++ src/test/java/one/digitalinnovation/beerstock/service/BeerServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -40,6 +41,13 @@ class BeerServiceTest {
         Beer expectedSavedBeer = beerMapper.toModel(expectedDTO);
 
         // when
+        // When static method from Mockito is called, we are saying that when a given method is
+        // called, then Mockito returns a given result.
+        // Checking two 'when' method calls below:
+        //  1. when method findByName from BeerRepository interface is called, then Mockito returns
+        //      an empty optional object.
+        //  2. when method save from BeerRepository is called, then it must return the exact object
+        //      that was passed via parameter on 'save'.
         when(beerRepository.findByName(expectedDTO.getName())).thenReturn(Optional.empty());
         when(beerRepository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
 
@@ -62,4 +70,16 @@ class BeerServiceTest {
         assertThat(createdBeerDTO.getQuantity(), is(greaterThan(2)));
     }
 
+    @Test
+    void whenAlreadyRegisteredBeerInformedThenAnExceptionShouldBeThrown() {
+        // given an informed beer
+        BeerDTO expectedBeerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer duplicatedBeer = beerMapper.toModel(expectedBeerDTO);
+
+        // when
+        when(beerRepository.findByName(expectedBeerDTO.getName())).thenReturn(Optional.of(duplicatedBeer));
+
+        // then
+        assertThrows(BeerAlreadyRegisteredException.class, () -> beerService.createBeer(expectedBeerDTO));
+    }
 }
