1f6bc3b6e7400e477f0fa4d04794468b47a6ace9
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-03-17 22:06:51 -0300

    create beer endpoint

---
+++ src/test/java/one/digitalinnovation/beerstock/controller/BeerControllerTest.java
@@ -0,0 +1,70 @@
+package one.digitalinnovation.beerstock.controller;
+
+import one.digitalinnovation.beerstock.builder.BeerDTOBuilder;
+import one.digitalinnovation.beerstock.controllers.BeerController;
+import one.digitalinnovation.beerstock.domains.dtos.BeerDTO;
+import one.digitalinnovation.beerstock.infrastructure.exceptions.BeerAlreadyRegisteredException;
+import one.digitalinnovation.beerstock.services.BeerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static one.digitalinnovation.beerstock.utils.JsonConversionUtils.asJsonString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.core.Is.is;
+
+@ExtendWith(MockitoExtension.class)
+class BeerControllerTest {
+
+    private static final String BEER_API_URL_PATH = "/api/v1/beers";
+    private static final long VALID_BEER_ID = 1L;
+    private static final long INVALID_BEER_ID = 2L;
+    private static final String BEER_API_SUBPATH_INCREMENT_URL = "/api/v1/beers";
+    private static final String BEER_API_SUBPATH_DECREMENT_URL = "/api/v1/beers";
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private BeerService beerService;
+
+    @InjectMocks
+    private BeerController beerController;
+
+    @BeforeEach
+    void setUp() {
+//        TODO study each builder method
+        mockMvc = MockMvcBuilders.standaloneSetup(beerController)
+            .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+            .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
+            .build();
+    }
+
+    @Test
+    void whenPOSTIsCalledThenABeerIsCreated() throws Exception {
+        // given
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+        // when
+        when(beerService.createBeer(beerDTO)).thenReturn(beerDTO);
+
+        // then
+        mockMvc.perform(post(BEER_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(beerDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is(beerDTO.getName())))
+                .andExpect(jsonPath("$.brand", is(beerDTO.getBrand())))
+                .andExpect(jsonPath("$.beerType", is(beerDTO.getBeerType().toString())));
+    }
+}
